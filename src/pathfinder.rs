292a85836843b8689d1b883aa67a4ae16e71157c
Nicolas Bonnet <mail@nicolasbon.net>
2020-02-20 19:00:16 +0100

    Refactor pathfinder to remove start and end from stored variables

--- src/pathfinder.rs
+++ src/pathfinder.rs
@@ -9,28 +9,25 @@ pub(crate) fn find_path<'a>(obstacles: ArrayView2<'a, bool>, start: &'a Ix2, end
     if obstacles[*start] || obstacles[*end] {
         return Err(Box::new(NoPathFoundError()));
     }
-    Pathfinder::new(obstacles, start, end).find_path()
+    Pathfinder::new(obstacles).find_path(end, start)
 }
 
 struct Pathfinder<'a> {
     obstacles: ArrayView2<'a, bool>,
-    start: &'a Ix2,
-    end: &'a Ix2,
-
     open_set: BinaryHeap<HeapElement>,
     came_from: Array2<Option<Ix2>>,
     g_score: Array2<f64>,
     f_score: Array2<f64>,
 }
 
-fn distance(a: &Ix2, b: &Ix2) -> i32 {
+fn taxicab_distance(a: &Ix2, b: &Ix2) -> i32 {
     let (ax, ay) = (a[0] as i32, a[1] as i32);
     let (bx, by) = (b[0] as i32, b[1] as i32);
     return (ax - bx).abs() + (ay - by).abs();
 }
 
 impl<'a> Pathfinder<'a> {
-    fn new(obstacles: ArrayView2<'a, bool>, start: &'a Ix2, end: &'a Ix2) -> Pathfinder<'a> {
+    fn new(obstacles: ArrayView2<'a, bool>) -> Pathfinder<'a> {
         let open_set = BinaryHeap::with_capacity(1024);
         let came_from = Array::from_elem(obstacles.raw_dim(), None);
         let g_score = Array::from_elem(obstacles.raw_dim(), INFINITY);
@@ -38,8 +35,6 @@ impl<'a> Pathfinder<'a> {
 
         Pathfinder {
             obstacles,
-            start:end, // Swapping start and end for more efficient path reconstruction.
-            end:start,
             open_set,
             came_from,
             g_score,
@@ -48,32 +43,29 @@ impl<'a> Pathfinder<'a> {
     }
 
 
-    fn h(&self, position: &Ix2) -> f64 {
-        return distance(self.end, position) as f64;
-        //return 0.;
-    }
+    fn find_path(&mut self, start: &Ix2, end: &Ix2) -> Result<Vec<Ix2>, Box<dyn std::error::Error>> {
+        let heuristic = |pos: &Ix2| taxicab_distance(pos, end) as f64;
 
-    fn find_path(&mut self) -> Result<Vec<Ix2>, Box<dyn std::error::Error>> {
         self.open_set.push(HeapElement {
-            position: *self.start,
-            f_score: self.h(self.start),
+            position: *start,
+            f_score: heuristic(start),
         });
-        self.g_score[*self.start] = 0.;
-        self.f_score[*self.start] = self.h(self.start);
+        self.g_score[*start] = 0.;
+        self.f_score[*start] = heuristic(start);
 
         while let Some(HeapElement { position, f_score }) = self.open_set.pop() {
             if f_score != self.f_score[position] { continue; }
 
-            self.visit(position);
+            self.visit(position, heuristic);
 
-            if position == *self.end {
-                return Ok(self.build_path());
+            if position == *end {
+                return Ok(self.build_path(start, end));
             }
         }
         return Err(Box::new(NoPathFoundError()));
     }
 
-    fn visit(&mut self, current: Ix2) {
+    fn visit<Func: Fn(&Ix2) -> f64>(&mut self, current: Ix2, heuristic: Func) {
         for neighbor in get_neighbors(&current, self.obstacles.dim()) {
             if self.obstacles[neighbor] { continue; }
             let tentative_g_score = self.g_score[current] + 1.;
@@ -81,18 +73,17 @@ impl<'a> Pathfinder<'a> {
                 self.came_from[neighbor] = Some(current);
                 self.g_score[neighbor] = tentative_g_score;
 
-                let f_score = tentative_g_score + self.h(&neighbor);
+                let f_score = tentative_g_score + heuristic(&neighbor);
                 self.f_score[neighbor] = f_score;
                 self.open_set.push(HeapElement { position: neighbor, f_score });
             }
         };
     }
 
-    fn build_path(&self) -> Vec<Ix2> {
-        let end = *self.end;
-        let mut path = vec![end];
-        let mut pos = end;
-        while pos != *self.start {
+    fn build_path(&self, start: &Ix2, end: &Ix2) -> Vec<Ix2> {
+        let mut path = vec![*end];
+        let mut pos = *end;
+        while pos != *start {
             pos = self.came_from[pos].unwrap();
             path.push(pos);
         }
