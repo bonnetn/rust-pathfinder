f5361b3813701253212da02074b1f416bb1b1fca
Nicolas Bonnet <mail@nicolasbon.net>
2020-05-01 20:42:38 +0200

    Optimize LOS calculation

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub use crate::find_path::find_path_impl;
 pub use crate::grid::Grid;
 pub use crate::map::{Bounded, GridMap};
 use crate::point::{is_in_bounds, Point2D};
+use crate::map::GridMap2;
 
 mod errors;
 mod exit_red_zone;
@@ -25,6 +26,32 @@ pub fn find_path(obstacles: &PyArray2<bool>, start: Point2D, end: Point2D) -> Py
     let obstacles = obstacles.to_owned_array();
     let obstacles = Grid::from(obstacles);
     let map = GridMap::new(obstacles);
+
+    if !is_in_bounds(start, map.boundaries()) {
+        return Err(exceptions::ValueError::py_err("start position not in bounds".to_string()));
+    }
+
+    if !is_in_bounds(end, map.boundaries()) {
+        return Err(exceptions::ValueError::py_err("end position not in bounds".to_string()));
+    }
+
+    let result = match find_path_impl(&map, start, end) {
+        Ok(r) => r,
+        Err(e) => return Err(exceptions::RuntimeError::py_err(e.to_string())),
+    };
+    let result = result
+        .iter()
+        .map(|p| *p)
+        .collect();
+
+    Ok(result)
+}
+
+#[pyfunction]
+pub fn find_path2(obstacles: &PyArray2<bool>, circles: Vec<(Point2D, isize)>, start: Point2D, end: Point2D) -> PyResult<Vec<Point2D>> {
+    let obstacles = obstacles.to_owned_array();
+    let obstacles = Grid::from(obstacles);
+    let map = GridMap2::new(obstacles, circles);
 
     if !is_in_bounds(start, map.boundaries()) {
         return Err(exceptions::ValueError::py_err("start position not in bounds".to_string()));
@@ -67,6 +94,7 @@ pub fn exit_red_zone(obstacles: &PyArray2<bool>, start: Point2D) -> PyResult<Poi
 fn grid_pathfinding(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(exit_red_zone))?;
     m.add_wrapped(wrap_pyfunction!(find_path))?;
+    m.add_wrapped(wrap_pyfunction!(find_path2))?;
 
     Ok(())
 }
--- src/map.rs
+++ src/map.rs
@@ -1,6 +1,6 @@
 use crate::grid::Grid;
-use crate::point::Point2D;
 use crate::line_of_sight::line_of_sight;
+use crate::point::Point2D;
 
 pub trait Bounded {
     fn boundaries(&self) -> (Point2D, Point2D);
@@ -32,6 +32,97 @@ impl Map for GridMap {
 }
 
 impl Bounded for GridMap {
+    fn boundaries(&self) -> ((isize, isize), (isize, isize)) {
+        return self.obstacles.boundaries();
+    }
+}
+
+pub struct GridMap2 {
+    obstacles: Grid<bool>,
+    circles: Vec<(Point2D, isize)>,
+}
+
+impl GridMap2 {
+    pub fn new(obstacles: Grid<bool>, circles: Vec<(Point2D, isize)>) -> GridMap2 {
+        GridMap2 { obstacles, circles }
+    }
+}
+
+fn norm2(x: isize, y: isize) -> isize {
+    return x * x + y * y;
+}
+
+fn sub((ax, ay): (isize, isize), (bx, by): (isize, isize)) -> (isize, isize) {
+    return (ax - bx, ay - by);
+}
+
+impl Map for GridMap2 {
+    fn obstacle(&self, point: &(isize, isize)) -> bool {
+        self.obstacles.get(point)
+    }
+
+    fn line_of_sight(&self, start: &(isize, isize), end: &(isize, isize)) -> bool {
+        // intersection = DirRay * t + StarRay
+        // || intersection - circlePos || = radius
+
+        // intersection - circlePos = DirRay * t + StarRay - circlePos
+
+        // || intersection - circlePos ||^2 = radius^2 = (intersection - circlePos) . (intersection - circlePos)
+
+        // radius^2 = (DirRay * t + StartRay - CirclePos)(DirRay * t + StarRay - CirclePos)
+        // Alpha = StartRay - CirclePos
+
+        // radius^2 = (DirRay * t + Alpha)(DirRay * t + Alpha)
+
+        // radius^2 = t^2 * || DirRay || ^ 2 + 2 * t * (DirRay.Alpha) + || Alpha || ^ 2
+        // 0 = t^2 * || DirRay || ^ 2 + 2 * t * (DirRay.Alpha) + || Alpha || ^ 2 - radius ^ 2
+
+        // A = || DirRay || ^ 2
+        // B = 2 * (DirRay.Alpha)
+        // C = || Alpha || ^ 2 - radius ^ 2
+
+        // Delta = B^2 - 4 * A * C
+        // X1 = (-B-sqrt(Delta))/2/A
+        // X2 = (-B+sqrt(Delta))/2/A
+
+        let (dir_ray_x, dir_ray_y) = sub(*end, *start);
+        let norm2_dir_ray = norm2(dir_ray_x, dir_ray_y);
+
+        for (point, radius) in &self.circles {
+            let (alpha_x, alpha_y) = sub(*start, *point);
+            let norm2_alpha = norm2(alpha_x, alpha_y);
+            let dir_ray_dot_start = dir_ray_x * alpha_x + dir_ray_y * alpha_y;
+
+            let a = norm2_dir_ray;
+            let b = 2 * dir_ray_dot_start;
+            let c = norm2_alpha - radius * radius;
+
+            let a = a as i64;
+            let b = b as i64;
+            let c = c as i64;
+
+            let delta = b * b - 4 * a * c;
+            if delta < 0 {
+                continue; // Ray does not hit this obstacle.
+            }
+
+            let delta_f64 = delta as f64;
+            let delta_f64_sqrt = delta_f64.sqrt();
+            let x1 = (-(b as f64) - delta_f64_sqrt) / 2. / (a as f64);
+            if x1 > 0. && x1 < 1. {
+                return false; // Ray hit the obstacle, no LOS;
+            }
+
+            let x2 = (-(b as f64) + delta_f64_sqrt) / 2. / (a as f64);
+            if x2 > 0. && x2 < 1. {
+                return false; // Ray hit the obstacle, no LOS;
+            }
+        }
+        return true;
+    }
+}
+
+impl Bounded for GridMap2 {
     fn boundaries(&self) -> ((isize, isize), (isize, isize)) {
         return self.obstacles.boundaries();
     }
